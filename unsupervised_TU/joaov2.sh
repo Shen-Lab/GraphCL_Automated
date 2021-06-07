67475cf6cb06875ec24c0f45f746922cfdba993d
Yuning You <46954127+yyou1996@users.noreply.github.com>
2021-06-06 20:15:33 -0500

    Update joaov2.sh

--- unsupervised_TU/joaov2.sh
+++ unsupervised_TU/joaov2.sh
@@ -2,6 +2,5 @@
 
 for seed in 0 1 2 3 4 
 do
-  CUDA_VISIBLE_DEVICES=$1 python gsimclr_minmax_condition.py --DS $2 --lr 0.001 --local --num-gc-layers 3 --aug minmax --gamma $3 --seed $seed
+  python gsimclr_minmax_condition.py --DS $1 --lr 0.001 --local --num-gc-layers 3 --aug minmax --gamma $2 --seed $seed
 done
-
