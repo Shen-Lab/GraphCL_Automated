9acfc8705c29e130f5cb8dfa95292c64b179d995
Yuning You <46954127+yyou1996@users.noreply.github.com>
2021-08-18 19:23:17 -0500

    Update joao.sh

--- unsupervised_TU/joao.sh
+++ unsupervised_TU/joao.sh
@@ -2,6 +2,6 @@
 
 for seed in 0 1 2 3 4 
 do
-  python gsimclr_minmax.py --DS $1 --lr 0.01 --local --num-gc-layers 3 --aug minmax --gamma $2 --seed $seed
+  python joao.py --DS $1 --lr 0.01 --local --num-gc-layers 3 --aug minmax --gamma $2 --seed $seed
 done
 
